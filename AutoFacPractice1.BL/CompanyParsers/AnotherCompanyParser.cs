f29a458f714e37ff8726e7c6f97049d0956496d5
Gogs <gogs@fake.local>
2019-01-23 15:47:04 +0100

    Instance count

--- AutoFacPractice1.BL/CompanyParsers/AnotherCompanyParser.cs
+++ AutoFacPractice1.BL/CompanyParsers/AnotherCompanyParser.cs
@@ -8,11 +8,20 @@ namespace AutoFacPractice1.BL.CompanyParsers
 {
     public class AnotherCompanyParser : CompanyParser
     {
+        static int counter = 1;
+        
+        public AnotherCompanyParser()
+        {
+            id = counter++;
+        }
+
+        int id;
+
         public override string Parse(string userName)
         {
             string r = "";
             if (userName.Length > 3) r = userName.Substring(userName.Length - 2, 1);
-            return r;
+            return String.Format("{0} - parser instance {1}", r, id);
         }
     }
 }
